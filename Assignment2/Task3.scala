f12412c5bcd95a2df45d7aa05918c6e3aaf2118f
EIivaras <zach00182@gmail.com>
2020-06-25 13:36:59 -0400

    Task 3 complete

--- Assignment2/Task3.scala
+++ Assignment2/Task3.scala
@@ -1,6 +1,11 @@
 import org.apache.spark.{SparkContext, SparkConf}
 
 // please don't change the object name
+
+// For each user, output the user's column number and the total number of ratings for that user
+// If a user has no ratings, then output 0 for that user
+// Ex/ 1,2 // user 1 has 2 ratings
+
 object Task3 {
   def main(args: Array[String]) {
     val conf = new SparkConf().setAppName("Task 3")
@@ -8,9 +13,17 @@ object Task3 {
 
     val textFile = sc.textFile(args(0))
 
-    // modify this code
-    val output = textFile.map(x => x);
-    
-    output.saveAsTextFile(args(1))
+    val result =  sc.textFile(args(0))
+                   .map(line => line.split(','))
+                   .map(line => line.drop(1))
+                   .map(line => line.map(x => if (x == "") "0" else "1"))
+                   .map(line => line.map(_.toInt))
+                   .collect.transpose
+                   .map(_.sum)
+                   .zipWithIndex
+                   .map{ case (value, index) => s"${index+1},$value"}
+
+    // TODO: All correct but need to not print out brackets (due to tuple)
+    sc.parallelize(result).saveAsTextFile(args(1))
   }
 }
