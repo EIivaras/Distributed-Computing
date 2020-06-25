120880768078beb0d29d4ddecc5e1c34e66f7c65
Tarek98 <tabdelme@edu.uwaterloo.ca>
2020-06-25 17:52:12 -0400

    finish task1 hadoop

--- Assignment2/Task1.java
+++ Assignment2/Task1.java
@@ -1,9 +1,11 @@
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.StringTokenizer;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.ArrayWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -12,11 +14,60 @@ import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 
+// Goal:
+// -For each movie, output the column numbers of the of users who gave the highest ratings
+// -If the highest rating is given by multiple users, output all of them in ascending numerical order
+// -Assume that there is at least one non-blank rating for each movie
+
+// NOTE:
+// There is no need for a reducer here, as each record can be iterated over once to get the result (directly from Mapper)
+
 public class Task1 {
-
-  // add code here
-
+  public static class Task1Mapper extends Mapper<Object, Text, Text, Text> {
+    private Text result;
+    private Text movieTitle = new Text();
     
+    public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+      String[] tokens = value.toString().split(",");
+      result = new Text();
+      movieTitle.set(tokens[0]);
+
+      int maxRating = 1;
+      // find maxRating for the movie
+      for (int i = 1; i < tokens.length; i++) {
+        if (!tokens[i].equals(null) && !tokens[i].equals("")) {
+          try {
+            int tmp = Integer.parseInt(tokens[i]);
+            if (tmp > maxRating) {
+              maxRating = tmp;
+              if (tmp == 5) break; // highest max rating possible is 5
+            }
+          } catch (NumberFormatException e) {
+            continue;
+          }
+        }
+      }
+
+      // find all user columns with the maxRating
+      for (int i = 1; i < tokens.length; i++) {
+        if (!tokens[i].equals(null) && !tokens[i].equals("")) {
+          try {
+            int tmp = Integer.parseInt(tokens[i]);
+            if (tmp == maxRating) {
+              if (result.getLength() != 0) {
+                result.append(",".getBytes(StandardCharsets.UTF_8), 0, 1);
+              }
+              result.append(Integer.toString(i).getBytes(StandardCharsets.UTF_8), 0, Integer.toString(i).length());
+            }
+          } catch (NumberFormatException e) {
+            continue;
+          }
+        }
+      }
+
+      context.write(movieTitle, result);
+    }
+  }   
     
   public static void main(String[] args) throws Exception {
     Configuration conf = new Configuration();
@@ -27,7 +78,12 @@ public class Task1 {
 
     String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
     
-    // add code here
+    job.setMapperClass(Task1Mapper.class);
+
+    job.setMapOutputKeyClass(Text.class);
+    job.setMapOutputValueClass(Text.class);
+
+    job.setNumReduceTasks(0);
 
     TextInputFormat.addInputPath(job, new Path(otherArgs[0]));
     TextOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
