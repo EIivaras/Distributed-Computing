53885f5f5b8e1b2e0129204187751d98bf841b3d
Tarek98 <tabdelme@edu.uwaterloo.ca>
2020-06-25 22:21:34 -0400

    Task1 scala draft

--- Assignment2/Task1.scala
+++ Assignment2/Task1.scala
@@ -8,8 +8,28 @@ object Task1 {
 
     val textFile = sc.textFile(args(0))
 
-    // modify this code
-    val output = textFile.map(x => x);
+    val movieTitles = textFile.map(line => line.split(","))
+                              .map(line => line(0))
+                              .collect()
+
+    var output = textFile.map(line => line.split(","))
+                        .map(line => line.drop(1))
+                        .map(line => line.map(x => if (x == "") "0" else x))
+                        .map(line => line.map(x => x.toInt))
+                        .map(line => line.map(x => if (x == line.max) 5 else 0))
+                        .zipWithIndex
+                        // .map(line => line.zipWithIndex.filter { case (value, index)  => value == 5 })
+                        // .map(line => line(0)._2 + 1)
+                        // .map(line => line.filter(_.nonEmpty))
+    output.persist()
+    
+    // var maxRating = 1
+    // find maxRating
+    // val maxRating = output.map(_.max)
+    // output.map(line => line.max(r => if(r.toInt > maxRating) maxRating = r.toInt))
+
+    // output = output.map(line => line.zipWithIndex.filter{ case (value, index)  => value.toInt == maxRating })
+                  //  .map{ case (value, index) => index }
     
     output.saveAsTextFile(args(1))
   }
