2d086f87612e8946922dde2f823be7217281eb34
Tarek98 <tabdelme@edu.uwaterloo.ca>
2020-06-26 11:29:15 -0400

    finish task1 scala

--- Assignment2/Task1.scala
+++ Assignment2/Task1.scala
@@ -10,7 +10,6 @@ object Task1 {
 
     val movieTitles = textFile.map(line => line.split(","))
                               .map(line => line(0))
-                              .collect()
 
     var output = textFile.map(line => line.split(","))
                          .map(line => line.drop(1))
@@ -20,18 +19,7 @@ object Task1 {
                          .map(line => line.zipWithIndex.map { case (value, index) => Array(value, index) })
                          .map(line => line.filter(x => x(0) == 5))
                          .map(line => line.map(x => (x(1)+1).toString).mkString(","))
-
-    //output.persist()
-    
-    // var maxRating = 1
-    // find maxRating
-    // val maxRating = output.map(_.max)
-    // output.map(line => line.max(r => if(r.toInt > maxRating) maxRating = r.toInt))
-
-    // output = output.map(line => line.zipWithIndex.filter{ case (value, index)  => value.toInt == maxRating })
-                  //  .map{ case (value, index) => index }
-    
-    // sc.parallelize(output.first).saveAsTextFile(args(1))
-    output.saveAsTextFile(args(1))
+                         
+    movieTitles.zip(output).map{ case (title, users) => s"$title,$users" }.saveAsTextFile(args(1))
   }
 }
