f911486957e21a17de1435260a2b1100f7c4509c
EIivaras <zach00182@gmail.com>
2020-07-03 15:46:05 -0400

    Fixed task 2 outputting 2 files

--- Assignment2/Task2.scala
+++ Assignment2/Task2.scala
@@ -4,20 +4,16 @@ import org.apache.spark.{SparkContext, SparkConf}
 object Task2 {
   def main(args: Array[String]) {
     val conf = new SparkConf().setAppName("Task 2")
-    conf.set("spark.sql.shuffle.partitions", "1")
     val sc = new SparkContext(conf)
 
-    val result = sc.parallelize(
-                  Seq(
-                    sc.textFile(args(0))
-                    .map(line => line.split(','))
-                    .map(line => line.drop(1))
-                    .map(line => line.filter(_.nonEmpty))
-                    .map(line => line.map(_.toInt))
-                    .map(line => line.size)
-                    .reduce(_+_)
-                  )
-                 )
+    val result = sc.textFile(args(0))
+                   .map(line => line.split(','))
+                   .map(line => line.drop(1))
+                   .map(line => line.filter(_.nonEmpty))
+                   .map(line => line.map(_.toInt))
+                   .map(line => ("key", line.size))
+                   .reduceByKey(_+_, 1)
+                   .map{ case (k, v) => v }
 
     result.saveAsTextFile(args(1))
   }
