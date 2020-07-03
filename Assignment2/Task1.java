cd5bb363076030afdebbe6018c43b6a8ae86d0e0
Tarek98 <tabdelme@edu.uwaterloo.ca>
2020-07-02 22:54:19 -0400

    fix maximum value issue

--- Assignment2/Task1.java
+++ Assignment2/Task1.java
@@ -1,15 +1,11 @@
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.util.StringTokenizer;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.ArrayWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
-import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
