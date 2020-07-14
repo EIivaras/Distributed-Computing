eb5e3d64854b5f2a17cbdbfc79b9020e662bd872
EIivaras <zach00182@gmail.com>
2020-07-13 21:12:48 -0400

    Got server nodes spawning and connecting to ZK and fleshed out notes

--- Assignment3/StorageNode.java
+++ Assignment3/StorageNode.java
@@ -19,46 +19,57 @@ public class StorageNode {
     static Logger log;
 
     public static void main(String [] args) throws Exception {
-	BasicConfigurator.configure();
-	log = Logger.getLogger(StorageNode.class.getName());
+		BasicConfigurator.configure();
+		log = Logger.getLogger(StorageNode.class.getName());
 
-	if (args.length != 4) {
-	    System.err.println("Usage: java StorageNode host port zkconnectstring zknode");
-	    System.exit(-1);
+		if (args.length != 4) {
+			System.err.println("Usage: java StorageNode host port zkconnectstring zknode");
+			System.exit(-1);
+		}
+
+		CuratorFramework curClient =
+			CuratorFrameworkFactory.builder()
+			.connectString(args[2])
+			.retryPolicy(new RetryNTimes(10, 1000))
+			.connectionTimeoutMs(1000)
+			.sessionTimeoutMs(10000)
+			.build();
+
+		curClient.start();
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+			public void run() {
+				curClient.close();
+			}
+			});
+
+		KeyValueService.Processor<KeyValueService.Iface> processor = new KeyValueService.Processor<>(new KeyValueHandler(args[0], Integer.parseInt(args[1]), curClient, args[3]));
+		TServerSocket socket = new TServerSocket(Integer.parseInt(args[1]));
+		TThreadPoolServer.Args sargs = new TThreadPoolServer.Args(socket);
+		sargs.protocolFactory(new TBinaryProtocol.Factory());
+		sargs.transportFactory(new TFramedTransport.Factory());
+		sargs.processorFactory(new TProcessorFactory(processor));
+		sargs.maxWorkerThreads(64);
+		TServer server = new TThreadPoolServer(sargs);
+		log.info("Launching server");
+
+		new Thread(new Runnable() {
+			public void run() {
+				server.serve();
+			}
+		}).start();
+
+		// TODO: create an ephemeral node in ZooKeeper
+		// Child znode must store, as its data payload, a host:port string denoting the address of the server process
+		
+		// NOTE: Right now, args[0] is just "hostname" as specified in runserver.sh
+		// I imagine we need to change this or extract it some other way?
+		String payload = String.format("%s:%s", args[0], args[1]);
+		byte[] payloadInBytes = payload.getBytes();
+
+		// args[3] = /zwalford
+		// TODO: Does it matter if the path is "/parent" or "/parent/[someNameForTheChild]" when creating the child nodes? 
+		curClient.create()
+				.withMode(CreateMode.EPHEMERAL_SEQUENTIAL)
+				.forPath(args[3], payloadInBytes);
 	}
-
-	CuratorFramework curClient =
-	    CuratorFrameworkFactory.builder()
-	    .connectString(args[2])
-	    .retryPolicy(new RetryNTimes(10, 1000))
-	    .connectionTimeoutMs(1000)
-	    .sessionTimeoutMs(10000)
-	    .build();
-
-	curClient.start();
-	Runtime.getRuntime().addShutdownHook(new Thread() {
-		public void run() {
-		    curClient.close();
-		}
-	    });
-
-	KeyValueService.Processor<KeyValueService.Iface> processor = new KeyValueService.Processor<>(new KeyValueHandler(args[0], Integer.parseInt(args[1]), curClient, args[3]));
-	TServerSocket socket = new TServerSocket(Integer.parseInt(args[1]));
-	TThreadPoolServer.Args sargs = new TThreadPoolServer.Args(socket);
-	sargs.protocolFactory(new TBinaryProtocol.Factory());
-	sargs.transportFactory(new TFramedTransport.Factory());
-	sargs.processorFactory(new TProcessorFactory(processor));
-	sargs.maxWorkerThreads(64);
-	TServer server = new TThreadPoolServer(sargs);
-	log.info("Launching server");
-
-	new Thread(new Runnable() {
-		public void run() {
-		    server.serve();
-		}
-	    }).start();
-
-	// TODO: create an ephemeral node in ZooKeeper
-	// curClient.create(...)
-    }
 }
