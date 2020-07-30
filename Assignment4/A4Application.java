4a4ee3f5b5acabe8c1972a67dec6109ef3647cbf
EIivaras <zach00182@gmail.com>
2020-07-30 15:31:13 -0400

    starting work

--- Assignment4/A4Application.java
+++ Assignment4/A4Application.java
@@ -19,39 +19,60 @@ import java.util.Properties;
 public class A4Application {
 
     public static void main(String[] args) throws Exception {
-	// do not modify the structure of the command line
-	String bootstrapServers = args[0];
-	String appName = args[1];
-	String studentTopic = args[2];
-	String classroomTopic = args[3];
-	String outputTopic = args[4];
-	String stateStoreDir = args[5];
+		// do not modify the structure of the command line
+		String bootstrapServers = args[0];
+		String appName = args[1];
+		String studentTopic = args[2];
+		String classroomTopic = args[3];
+		String outputTopic = args[4];
+		String stateStoreDir = args[5];
 
-        Properties props = new Properties();
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, appName);
-        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-	props.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
-        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
-        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
-	props.put(StreamsConfig.STATE_DIR_CONFIG, stateStoreDir);
+		Properties props = new Properties();
+		props.put(StreamsConfig.APPLICATION_ID_CONFIG, appName);
+		props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+		props.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
+		props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+		props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+		props.put(StreamsConfig.STATE_DIR_CONFIG, stateStoreDir);
 
-	// add code here if you need any additional configuration options
+		// add code here if you need any additional configuration options
 
-        StreamsBuilder builder = new StreamsBuilder();
+		StreamsBuilder builder = new StreamsBuilder();
 
-	// add code here
-	// 
-        // ... = builder.stream(studentTopic);
-        // ... = builder.stream(classroomTopic);
-	// ...
-	// ...to(outputTopic);
+		// IDEA:
+		// Kafka operates similar to spark, in terms of how you put together your code (one long line)
+		// Drawing from word count, the idea would be to form a KTable based on the the lines of data that have already been read from the topic
+		// Then, we reduce/group/perform operations on that table much like how we did with spark
+		// However, because the input comes in a stream, every single time we have a new line of input, the table's values are re-calculated
+		// 		--> Don't worry though, b/c all previous values will remain the same: only new lines will be added, previous lines will not be changed
+		// By converting the KTable to a stream before writing to the output topic, we ensure that only the changes between tables are published to the output topic
+		// Therefore, we operate in a stream!
 
-        KafkaStreams streams = new KafkaStreams(builder.build(), props);
+		// Note: 
+		// --> studentLines = a bunch of KV pairs of the form "Student_ID,Room_ID", which is an event log of students walking between rooms
+		// --> classroomLines = a bunch of KV pairs of the form "Room_ID,Capacity", which is an event log of each room's capacity, which can change dynamically
+		
+		// Note: As in the lecture notes, if we "count" or something similar, make sure we do the counting in a stateful store
+		// Example does it like so:
+		// --> .count(Materialized.<String, Long, KeyValueStore<Bytes, byte[]>>as("counts-store"));
 
-	// this line initiates processing
-	streams.start();
+		// The real question is -- how do we manage with 2 streams? Stitching it together?
+		// Looks like we can join KStreams together? Maybe this is an approach we want to take?
+		// --> https://kafka.apache.org/21/javadoc/org/apache/kafka/streams/kstream/KStream.html
 
-	// shutdown hook for Ctrl+C
-	Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
+		KStream<String, String> studentLines = builder.stream(studentTopic);
+		KStream<String, String> classroomLines = builder.stream(classroomTopic);
+
+		KTable<String, String> events = 
+		// ...
+		// ...to(outputTopic);
+
+		KafkaStreams streams = new KafkaStreams(builder.build(), props);
+
+		// this line initiates processing
+		streams.start();
+
+		// shutdown hook for Ctrl+C
+		Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
     }
 }
